fe2d2649c660570c8615748486a0e28d0550f92f
divkovic <dragoslav.m.ivkovic@gmail.com>
2025-02-11 21:37:48 +0100

    footer

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
 
-import initializeBundleAnalyzer from '@next/bundle-analyzer';
-
-// https://www.npmjs.com/package/@next/bundle-analyzer
-const withBundleAnalyzer = initializeBundleAnalyzer({
-    enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true'
-});
-
-// https://nextjs.org/docs/pages/api-reference/next-config-js
-const nextConfig: NextConfig = {
+const nextConfig = {
     output: 'standalone'
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
